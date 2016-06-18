25833463c92b51e826f79ec3127aae243022e961
Jason <init6@init6.me>
2016-06-18 16:30:19 -0500

    update

--- ftc.py
+++ ftc.py
@@ -7,12 +7,12 @@ from faker import Faker
 
 #all his domains are here: http://pastie.org/private/7vibznjr5e75j1wxddyglq
 
-urls = ('apple-ios-icloud-verify.com 02246.poruzstw.cn 22i28.poruzstw.cn 40624.rjmolgbl.cn 02460.khfnwbzb.cn 202i2s.mupijeri.cn 46266.mupijeri.cn 4ukqy.ldxqlwyu.cn 600844.icfzjyyk.cn 64468.mupijeri.cn 662268.ckspbfms.cn 68066.poruzstw.cn 6a400.icfzjyyk.cn 6iqg8.rjmolgbl.cn 800420.mupijeri.cn 82480.icfzjyyk.cn 8288u.brhlbskz.cn 84868.rjmolgbl.cn 8gko88.rjmolgbl.cn 8ikig.ajegujwg.cn a64g0.ldxqlwyu.cn e8mk8y.mrpiwewm.cn ekqc48.poruzstw.cn emkuq.khfnwbzb.cn g22qmi.ckspbfms.cn gwogy4.mrpiwewm.cn wyei8e.ajegujwg.cn y46i8g.icfzjyyk.cn q68ium.baike.icfzjyyk.cn us6yo.brhlbskz.cn yy6ig.poruzstw.cn').split()
+urls = ('02246.poruzstw.cn 22i28.poruzstw.cn 40624.rjmolgbl.cn 02460.khfnwbzb.cn 202i2s.mupijeri.cn 46266.mupijeri.cn 4ukqy.ldxqlwyu.cn 600844.icfzjyyk.cn 64468.mupijeri.cn 662268.ckspbfms.cn 68066.poruzstw.cn 6a400.icfzjyyk.cn 6iqg8.rjmolgbl.cn 800420.mupijeri.cn 82480.icfzjyyk.cn 8288u.brhlbskz.cn 84868.rjmolgbl.cn 8gko88.rjmolgbl.cn 8ikig.ajegujwg.cn a64g0.ldxqlwyu.cn e8mk8y.mrpiwewm.cn ekqc48.poruzstw.cn emkuq.khfnwbzb.cn g22qmi.ckspbfms.cn gwogy4.mrpiwewm.cn wyei8e.ajegujwg.cn y46i8g.icfzjyyk.cn q68ium.baike.icfzjyyk.cn us6yo.brhlbskz.cn yy6ig.poruzstw.cn').split()
 
 #FuckTheChinese
-def ftc(username, password):
+def ftc(url, username, password):
         try:
-                url = 'http://%s/Home/Save' % (choice(urls))
+                url = 'http://%s/Home/Save' % (url)
                 data = { 'u' : username, 'p' : password }
                 print(url)
                 print(data)
@@ -52,4 +52,5 @@ if __name__ == "__main__":
         f = fp.readlines()
 
         while True:
-                ftc(fake_address(), choice(f).strip('\n'))
+        	for url in urls:
+        		ftc(url, fake_address(), choice(f).strip('\n'))
